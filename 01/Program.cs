6bdc187fe98f165813723f718039c4a1c7204f81
mariya <mariyakudryashova19980308@gmail.com>
2023-07-19 19:33:07 +0300

    add01

---
+++ 01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+for (int i = 1; i < number; i++)
+{
+    Console.Write($"{i} + ");
+    sum += i;
+}
+Console.Write("\b\b= ");
+Console.WriteLine(sum);
