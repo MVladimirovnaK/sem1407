017a5e72a25235d6732b88ec284044ab006945a2
mariya <mariyakudryashova19980308@gmail.com>
2023-07-19 20:07:39 +0300

    add03

---
+++ 03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int product = 1;
+
+for (int i = 1; i < number; i++)
+{
+    Console.Write($"{i} * ");
+    product *= i;
+}
+Console.Write("\b\b= ");
+Console.WriteLine(product);
