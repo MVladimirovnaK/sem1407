0b908722a583274dfcbd753d1d95c4d79ec63296
mariya <mariyakudryashova19980308@gmail.com>
2023-07-19 21:02:51 +0300

    add04

---
+++ 04/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+int size = 8;
+Random rnd = new Random();
+int[] numbers = new int[size];
+
+
+for (int i = 0; i < size; i++)
+{
+    numbers[i] = rnd.Next(0, 2);
+    Console.Write($"{numbers[i]} ");
+}
+Console.WriteLine();
